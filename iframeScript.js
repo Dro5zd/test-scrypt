cd2f412572da92a6b9793071f6be3d031d8d748c
Drobotiuk Andrii <92302246+Dro5zd@users.noreply.github.com>
2024-05-02 20:08:10 +0500

    Update iframeScript.js 3

--- iframeScript.js
+++ iframeScript.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const scriptSearchParams = new URLSearchParams(window.location.search);
+  const scriptElement = document.getElementById('rh-module-script');
 
- const customersAffiliateCode = scriptSearchParams.get('affiliateCode') || 'QPFV487OTHF0';
-  const iframePlacementId = scriptSearchParams.get('iframePlacementId') || 'myContainer';
+// Отримання значення атрибута data-affiliate-code
+  const customersAffiliateCode = scriptElement.getAttribute('data-affiliate-code');
 
-  const checkContainerInterval = setInterval(() => {
-    const container = document.getElementById(iframePlacementId);
-    if (container) {
-      container.appendChild(iframe);
-        // iframe.contentWindow.postMessage(parentDomain, 'http://localhost:3000/');
-      clearInterval(checkContainerInterval);
-    }
-  }, 200)
+// Отримання значення атрибута data-iframe-placement-id
+  const iframePlacementId = scriptElement.getAttribute('data-iframe-placement-id');
+
+  console.log(scriptElement, customersAffiliateCode, iframePlacementId);
+
+
+
+
+  // const queryString = window.location.search;
+  // const scriptSearchParams = new URLSearchParams(queryString);
+  // console.log('queryString test', queryString)
+  // console.log('scriptSearchParams test', scriptSearchParams)
+
+  // const customersAffiliateCode = scriptSearchParams.get('affiliateCode');
+  // const customersAffiliateCode = document.currentScript;
+  console.log('customersAffiliateCode test', customersAffiliateCode)
+  // const iframePlacementId = scriptSearchParams.get('iframePlacementId');
+  // const iframePlacementId = document.currentScript.getAttribute('data-iframe-placement-id');
 
   const parentDomain = window.location.origin;
 
@@ -28,11 +38,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const searchParams = objectToSearchParams(customersAffiliateCode);
 
- const urlWithSettings = `http://localhost:3002/?${searchParams}`;
+  const urlWithSettings = `http://localhost:3002/?${searchParams}`;
 
   const iframe = document.createElement('iframe');
+  // iframe.src = 'http://localhost:3002/';
   iframe.src = urlWithSettings;
   iframe.style.width = '100%';
   iframe.style.height = '100%';
   iframe.style.border = '0';
+
+  const checkContainerInterval = setInterval(() => {
+    const container = document.getElementById(iframePlacementId);
+    if (container) {
+      container.appendChild(iframe);
+      iframe.contentWindow.postMessage(parentDomain, 'http://localhost:3000/');
+      // iframe.contentWindow.postMessage(searchParams, 'https://module5.rhelpers.com/');
+      clearInterval(checkContainerInterval);
+    }
+  }, 200)
+
+  // iframe.addEventListener('load', function () {
+  //   // iframe.contentWindow.postMessage(parentDomain, 'https://module5.rhelpers.com/');
+  //   // console.log('parentDomain test шт ищч', parentDomain)
+  //   // iframe.contentWindow.postMessage(parentDomain, 'http://localhost:3002/');
+  // });
 });
