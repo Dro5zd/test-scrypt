e899e0aa604d6b3371b2d24f6f720b6023cd15db
Drobotiuk Andrii <92302246+Dro5zd@users.noreply.github.com>
2024-05-03 12:28:38 +0500

    Update iframeScript.js 4

--- iframeScript.js
+++ iframeScript.js
@@ -1,47 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const scriptElement = document.getElementById('rh-module-script');
-
-// Отримання значення атрибута data-affiliate-code
   const customersAffiliateCode = scriptElement.getAttribute('data-affiliate-code');
-
-// Отримання значення атрибута data-iframe-placement-id
   const iframePlacementId = scriptElement.getAttribute('data-iframe-placement-id');
-
-  console.log(scriptElement, customersAffiliateCode, iframePlacementId);
-
-
-
-
-  // const queryString = window.location.search;
-  // const scriptSearchParams = new URLSearchParams(queryString);
-  // console.log('queryString test', queryString)
-  // console.log('scriptSearchParams test', scriptSearchParams)
-
-  // const customersAffiliateCode = scriptSearchParams.get('affiliateCode');
-  // const customersAffiliateCode = document.currentScript;
-  console.log('customersAffiliateCode test', customersAffiliateCode)
-  // const iframePlacementId = scriptSearchParams.get('iframePlacementId');
-  // const iframePlacementId = document.currentScript.getAttribute('data-iframe-placement-id');
 
   const parentDomain = window.location.origin;
 
-  console.log('parentDomain test', parentDomain)
-
-  function objectToSearchParams(value) {
-    const params = new URLSearchParams();
-
-    params.append('affiliateCode', value);
-
-    return params;
-  }
-
-  const searchParams = objectToSearchParams(customersAffiliateCode);
-
-  const urlWithSettings = `http://localhost:3002/?${searchParams}`;
+  const urlWithSettings = `http://localhost:3002/?affiliateCode=${customersAffiliateCode}`;
 
   const iframe = document.createElement('iframe');
-  // iframe.src = 'http://localhost:3002/';
   iframe.src = urlWithSettings;
   iframe.style.width = '100%';
   iframe.style.height = '100%';
@@ -51,15 +18,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById(iframePlacementId);
     if (container) {
       container.appendChild(iframe);
-      iframe.contentWindow.postMessage(parentDomain, 'http://localhost:3000/');
-      // iframe.contentWindow.postMessage(searchParams, 'https://module5.rhelpers.com/');
+      console.log('parentDomain', parentDomain);
+      // відправляємо parentDomain на сервер
       clearInterval(checkContainerInterval);
     }
   }, 200)
 
-  // iframe.addEventListener('load', function () {
-  //   // iframe.contentWindow.postMessage(parentDomain, 'https://module5.rhelpers.com/');
-  //   // console.log('parentDomain test шт ищч', parentDomain)
-  //   // iframe.contentWindow.postMessage(parentDomain, 'http://localhost:3002/');
-  // });
 });
+
